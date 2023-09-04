92504e3d871a1f3d1c20dca42f69514a11fcd20b
Joseph Sagwe <josephsagwe421@gmail.com>
2023-09-04 09:39:22 +0300

    Update SpringbootTestApplicationTests.java

--- src/test/java/com/springboottest/SpringbootTestApplicationTests.java
+++ src/test/java/com/springboottest/SpringbootTestApplicationTests.java
@@ -3,8 +3,6 @@ package com.springboottest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-
 @SpringBootTest
 class SpringbootTestApplicationTests {
 
